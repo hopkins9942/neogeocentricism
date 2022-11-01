f3d9736bd166d176bce0a246e326198a880b0741
Matthew Hopkins <matthew.hopkins9942@gmail.com>
2022-11-01 15:10:11 +0000

    added transformation to geocentric ecliptic frame

--- main.py
+++ main.py
@@ -9,21 +9,56 @@
 
 import numpy as np
 
+import matplotlib.pyplot as plt
+
 import astropy.units as u
 from astropy.time import Time
 from astropy.coordinates import GCRS, get_body
 
 frame = GCRS()
 
-# ephemerides example use:
-tStart = Time('2014-12-25')
-tEnd = tStart + 1*u.yr
-t = np.linspace(tStart, tEnd)
-print(t)
-eph = get_body('moon', t)
-# returns array-like object with ra, dec and distance at each time
-print(eph)
-print(eph.ra, eph.dec)
+
+
+def xyz(eph):
+    """takes SkyCoord object in GCRS frame and coverts into x, y and z coords
+    in geocentric ecliptic frame
+    coord system has positive x axis along ra=0, dec=0, positive y in ecliptic plane
+    """
+    assert isinstance(eph.frame, GCRS)
+    
+    obl = 23.4*np.pi/180 # Earth axial tilt relative to ecliptic
+    
+    dist = eph.distance
+    ra = eph.ra
+    dec = eph.dec
+    
+    x = dist*np.cos(dec)*np.cos(ra)
+    y = dist*(np.cos(obl)*np.cos(dec)*np.sin(ra) + np.sin(obl)*np.sin(dec))
+    z = dist*(np.cos(obl)*np.sin(dec) - np.sin(obl)*np.cos(dec)*np.sin(ra))
+    
+    return(x,y,z)
+
+
+
+
+tStart = Time('2010-03-20')
+tEnd = tStart + 10*u.yr
+t = np.linspace(tStart, tEnd, 200)
+
+fig1, ax1 = plt.subplots()
+fig2, ax2 = plt.subplots()
+bodies = ['moon', 'sun', 'mercury', 'venus', 'mars', 'jupiter', 'saturn']
+for b in bodies:
+    eph = get_body(b, t)
+    x,y,z = xyz(eph)
+    ax1.plot(x, y, label=b)
+    ax2.plot(z, label=b)
+ax1.set_aspect('equal')
+ax1.legend()
+ax2.legend()
+
+
+
 
 
 class Model:
